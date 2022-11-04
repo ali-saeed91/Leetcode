f4f97f3d7954a47da2b30d627b049ad04474024f
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2022-11-04 20:12:00 +0500

    Time: 1597 ms (65.64%), Space: 20.5 MB (39.26%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,16 +3,12 @@ class Solution:
         maxHeap = []
         out = []
         (x1,y1) = (0,0)
-        # print(x1,y1)
         for x,y in points:
             distance = math.sqrt(((x-x1)**2) + ((y-y1)**2))
             maxHeap.append([distance,x,y])
-        # print(maxHeap)
         heapify(maxHeap)
-        # print(maxHeap)
         while k > 0:
             _,i,j = heappop(maxHeap)
             out.append([i,j])
-            # print(out)
             k -=1
         return out
