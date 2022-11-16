c9b1a69156a31f79b57e6171c1be41d03e2179b5
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2022-11-16 14:42:23 +0500

    Time: 39 ms (86.00%), Space: 14.1 MB (63.57%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        if s == goal:
+            return True if len(s) - len(set(s)) >= 1 else False
+        
+        diff =[]
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diff.append(i)
+                if len(diff) > 2:
+                    return False
+        if len(diff) != 2:
+            return False
+        if s[diff[0]] == goal[diff[1]] and s[diff[1]] == goal[diff[0]]:
+            return True
+        return False
