fada888e08ef13cd5c5ff020f961bdbe11dc80ac
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2022-11-17 14:42:55 +0500

    Time: 54 ms (58.28%), Space: 13.9 MB (69.08%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -5,19 +5,11 @@ class Solution:
         wordarr = text.split()
         print(wordarr)
         ln = len(wordarr)
-        print(ln)
+        # print(ln)
         out =""
         diff = 0 if ln == 1 else totalsp // (ln-1)
         addif = totalsp - diff * (ln -1)
        
         out = (' ' * diff).join(wordarr) + (' ' * addif)
-            # for word in wordarr:
-            #     if word != wordarr[-1]:
-            #         out += word + sp
-            #     if word == wordarr[-1]:
-            #         if addsp == ln:
-            #             out += word + " " 
-            #         if addsp > ln:
-            #             difadd = (addsp - ln) * " "
-            #             out += word + " " + difadd
+    
         return out
