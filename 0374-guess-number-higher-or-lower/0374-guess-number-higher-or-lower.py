febd9201c83911597b4b3c300b282826537db46a
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2022-11-22 23:29:47 +0500

    Time: 28 ms (96.99%), Space: 14 MB (15.97%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -7,4 +7,12 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        return __pick__
+        s = 1
+        e = n+1
+        while s+1<e:
+            mid = (s+e)//2
+            if guess(mid)>=0:
+                s = mid
+            else:
+                e = mid
+        return s
