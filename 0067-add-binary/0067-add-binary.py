aa4a3461a93ceefd851680efccf45daa501b9aaf
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2022-11-23 10:39:21 +0500

    Time: 78 ms (15.96%), Space: 13.9 MB (26.25%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,10 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        adecimal=(int(a,base=2))
-        bdecimal=(int(b,base=2))
-        c =adecimal+bdecimal
-        # print(c)
-        binc=(bin(c))
-        binc = binc[2:]
-        return binc
-       
+        carry = 0
+        result = ''
+
+        a = list(a)
+        b = list(b)
+
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
+
+            result += str(carry %2)
+            carry //= 2
+
+        return result[::-1]
