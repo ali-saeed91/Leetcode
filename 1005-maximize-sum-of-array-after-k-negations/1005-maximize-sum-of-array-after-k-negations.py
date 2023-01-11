8fd5fc9e6a4d962a63aaa23917c31c19379a9216
Ali Saeed <105937018+ali-saeed91@users.noreply.github.com>
2023-01-11 15:29:04 +0500

    Time: 38 ms (99.69%), Space: 13.9 MB (56.45%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         heapify(nums)
-        n=len(nums)
+    
         while k >0:
             x=heappop(nums)
             heappush(nums,-x)
